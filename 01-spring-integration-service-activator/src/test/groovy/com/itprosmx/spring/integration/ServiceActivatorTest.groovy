1bcc17278b07548a03789a40cb47cfcb2d35fd81
Felipe Gutierrez <felipeg@gmail.com>
2013-02-12 13:11:14 -0700

    Initial Import of a Spring Integration, using Service Activator

---
+++ 01-spring-integration-service-activator/src/test/groovy/com/itprosmx/spring/integration/ServiceActivatorTest.groovy
@@ -0,0 +1,14 @@
+package com.itprosmx.spring.integration
+
+import static org.junit.Assert.*
+
+import org.junit.Test
+
+class ServiceActivatorTest {
+
+	@Test
+	void serviceActivatorTest() {
+		fail("Not yet implemented");
+	}
+
+}
