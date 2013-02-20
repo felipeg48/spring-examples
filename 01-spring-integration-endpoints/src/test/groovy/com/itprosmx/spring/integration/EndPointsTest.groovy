f32f34c62bacf18cc40014f89a8e284d72ef55c3
Felipe Gutierrez <felipeg@gmail.com>
2013-02-20 13:50:38 -0700

    added the TEST file

---
+++ 01-spring-integration-endpoints/src/test/groovy/com/itprosmx/spring/integration/EndPointsTest.groovy
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package com.itprosmx.spring.integration
+
+import static org.junit.Assert.*
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
+
+/**
+ * @author felipeg
+ *
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("/META-INF/spring/endpoints-context.xml")
+class EndPointsTest {
+
+	@Test
+	void serviceActivatorTest() {
+		
+	}
+	
+	@Test
+	void messageBridgeTest() {
+		
+	}
+	
+	@Test
+	void messageEnricherTest() {
+		
+	}
+
+	@Test
+	void gatewaySynchronousTest() {
+		
+	}
+	@Test
+	void gatewayASynchronousTest() {
+		
+	}
+	
+	@Test
+	void delayerTest() {
+		
+	}
+}
