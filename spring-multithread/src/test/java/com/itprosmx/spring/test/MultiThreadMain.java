d59fc764ea577e4aec3838dc8a2dc3858accb0b2
felipeg48 <fgutierrezcru@vmware.com>
2013-09-03 13:33:51 -0500

    Simple example of multithreading using Spring

---
+++ spring-multithread/src/test/java/com/itprosmx/spring/test/MultiThreadMain.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package com.itprosmx.spring.test;
+
+import org.junit.Test;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+/**
+ * @author fgutierrezcru
+ *
+ */
+public class MultiThreadMain {
+	
+	@Test
+	public void test() {
+		ThreadPoolTaskExecutor task = new ThreadPoolTaskExecutor();
+		task.setCorePoolSize(5);
+		task.setWaitForTasksToCompleteOnShutdown(true);
+		task.afterPropertiesSet();
+		
+		Runnable run = new Runnable() {
+			public void run(){
+				try{
+					System.out.println(Thread.currentThread().getId());
+					Thread.sleep(500);
+				}catch(Exception ex){
+					ex.printStackTrace();
+				}
+			}
+		};
+		
+		for(int k=0; k<5; ++k) {
+			task.execute(run);
+		}
+	}
+
+}
