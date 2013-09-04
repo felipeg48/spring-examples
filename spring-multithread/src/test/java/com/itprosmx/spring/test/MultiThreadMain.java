9f160b1afc0c3c595eec024e7e5ebbb9bca910cd
felipeg48 <fgutierrezcru@vmware.com>
2013-09-04 15:10:17 -0500

    added a new test using the future class

--- spring-multithread/src/test/java/com/itprosmx/spring/test/MultiThreadMain.java
+++ spring-multithread/src/test/java/com/itprosmx/spring/test/MultiThreadMain.java
@@ -3,6 +3,14 @@
  */
 package com.itprosmx.spring.test;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
+
 import org.junit.Test;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
@@ -36,4 +44,40 @@ public class MultiThreadMain {
 		}
 	}
 
+	@Test
+	public void futureTest() throws InterruptedException, ExecutionException{
+		ThreadPoolTaskExecutor task = new ThreadPoolTaskExecutor();
+		task.setCorePoolSize(5);
+		task.setMaxPoolSize(5);
+		task.setWaitForTasksToCompleteOnShutdown(true);
+		task.afterPropertiesSet();
+		
+		
+		List<Future<Long>> tasks = new ArrayList<Future<Long>>();
+	    int max = 5;
+	    for (int i = 0; i < max; i++) {
+	        tasks.add(launch(task));
+	    }
+	    for (Future<Long> taskexec : tasks) {
+	        System.out.println("LAUNCH result : " + taskexec.get());
+	    }
+	}
+	
+	@SuppressWarnings("unchecked")
+	protected Future<Long> launch(ThreadPoolTaskExecutor taskExecutor) {
+	    return (Future<Long>) taskExecutor.submit(new FutureTask<Long>(new RunClass()));
+	}
+	
+	protected class RunClass implements Callable<Long>{
+		
+		public Long call() throws Exception {
+			
+			Integer sleep = new Random().nextInt(20000)+ 1000;
+			System.out.println("Before delay (" + sleep + "): " + Thread.currentThread().getId());
+			Thread.sleep(sleep);
+			System.out.println("After delay:" + Thread.currentThread().getId());
+			
+			return Thread.currentThread().getId();
+		}
+	}
 }
